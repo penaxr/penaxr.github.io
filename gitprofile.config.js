b13dcbb4c5b8e64cd919737d701d8dcaec29528b
Yann Savard <yannsd0728@gmail.com>
2022-04-25 07:30:42 -0400

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -6,8 +6,8 @@ const config = {
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
-      projects: ['laravel-ecommerce'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      forks: true, // Forked projects will not be displayed if set to true.
+      projects: [''], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
@@ -17,7 +17,7 @@ const config = {
     dribbble: '',
     behance: '',
     medium: '',
-    dev: '',
+    dev: 'penaxr',
     website: 'https://penaxr.github.io',
     phone: '',
     email: '',
@@ -58,7 +58,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'dark',
+    defaultTheme: 'business',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
