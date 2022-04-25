24d2c6b4f26c6dbcd202243c0a109d6bb303b376
Yann Savard <yannsd0728@gmail.com>
2022-04-25 13:00:25 -0400

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -47,7 +47,7 @@ const config = {
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
-    username: 'penaxr',
+    username: 'arifszn',
     limit: 5, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
